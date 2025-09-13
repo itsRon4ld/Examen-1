3f2b0e5662556e93f2c467b71930e3401a86596f
itsRon4ld <145395769+itsRon4ld@users.noreply.github.com>
2025-09-13 12:27:46 -0500

    Add files via upload

---
+++ Pregunta2/script.js
@@ -0,0 +1,13 @@
+
+
+
+
+function sumar() {
+    const num1 = document.getElementById('num1').value;
+
+
+    const resultado = (parseInt(num1)-32)*5/9
+
+    alert("La temperatura Calculada es resultado")
+
+}
